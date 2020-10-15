64e08df26bfb5f6ec319a36518de5e529e96e006
root <root@ip-172-31-36-114.us-east-2.compute.internal>
2020-10-15 15:09:29 +0000

    change db

--- techcoreua/settings.py
+++ techcoreua/settings.py
@@ -91,24 +91,24 @@ WSGI_APPLICATION = 'techcoreua.wsgi.application'
 
 # sqllite3 database connection
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# Postgresql database connection
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'techcoreuadb', 
-        'USER': 'techcoreua', 
-        'PASSWORD': 'pravuy14',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Postgresql database connection
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'techcoreuadb',
+#        'USER': 'techcoreua',
+#        'PASSWORD': 'pravuy14',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
 
 
 # Password validation
@@ -167,9 +167,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static_in_venv'),)
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 LOGIN_REDIRCT_URL = '/'
 
